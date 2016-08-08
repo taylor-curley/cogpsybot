65e396b9f9e6abc3a5fd5fbf182f0f76ee7c2e4a
Taylor C <tmc2737@gmail.com>
2016-08-08 17:42:22 -0400

    Update secret_cpb.py

--- secret_cpb.py
+++ secret_cpb.py
@@ -1,5 +1,5 @@
-# These bots will require communication with Google and Twitter's APIs. You will have to get your own keys!
+These bots will require communication with Google and Twitter's APIs. You will have to get your own keys!
 
-# For Twitter, you will need a Consumer Key, a Consumer Secret Key, an Owner ID, Access Token, and Access Token Secret.
+For Twitter, you will need a Consumer Key, a Consumer Secret Key, an Owner ID, Access Token, and Access Token Secret.
 
-# For Google's URL shortener, you will only need an API key.
+For Google's URL shortener, you will only need an API key.
